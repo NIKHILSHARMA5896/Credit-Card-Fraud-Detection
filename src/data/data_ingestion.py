0a0dc4e356954a204c646262dffc89868161dd36
B Nikhil Sharma <b.nikhil.sharma@gmail.com>
2025-06-10 13:15:55 +0530

    cicd-run-5

--- src/data/data_ingestion.py
+++ src/data/data_ingestion.py
@@ -10,23 +10,6 @@ from src.connections import s3_connection
 from dotenv import load_dotenv
 load_dotenv(override=True)
 
-def load_params(params_path: str) -> dict:
-    """Load parameters from a YAML file."""
-    try:
-        with open(params_path, 'r') as file:
-            params = yaml.safe_load(file)
-        logging.debug('Parameters retrieved from %s', params_path)
-        return params
-    except FileNotFoundError:
-        logging.error('File not found: %s', params_path)
-        raise
-    except yaml.YAMLError as e:
-        logging.error('YAML error: %s', e)
-        raise
-    except Exception as e:
-        logging.error('Unexpected error: %s', e)
-        raise
-
 def load_data(data_url: str) -> pd.DataFrame:
     """Load data from a CSV file."""
     try:
